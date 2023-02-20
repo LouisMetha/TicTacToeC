04363e125ba026e2a139d5dc907312725f631614
Louis Methaviriyavanich <louis.metha94@gmail.com>
2023-02-20 22:17:25 +1100

    Add project files.

---
+++ TTT.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+char board[3][3];
+
+void inti_board() {
+	int i, j;
+
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
+			board[i][j] = ' ';
+		}
+	}
+}
+
+void display() {
+
+}
+
+void checkGame() {
+
+}
